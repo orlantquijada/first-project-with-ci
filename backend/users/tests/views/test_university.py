ce41aa4c21fb199d5cfc82a5d3056b4948125be7
Orlan Quijada <orlantquijada@gmail.com>
2020-10-31 16:30:34 +0800

    added retrieve university test

--- backend/users/tests/views/test_university.py
+++ backend/users/tests/views/test_university.py
@@ -2,15 +2,16 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 
 from backend.generics.clients import StudentAPIClient
-from backend.users import factories
-from backend.users import models
+from backend.users.api.base import UniversityModelSerializer
+from backend.users.factories import UniversityFactory
+from backend.users.models import University
 
 
 class UniversityTests(APITestCase):
     client_class = StudentAPIClient
 
     url = "/api/v1/universities/"
-    factory = factories.UniversityFactory
+    factory = UniversityFactory
 
     def test_create_university(self):
         data = {"name": self.factory.name()}
@@ -24,7 +25,14 @@ class UniversityTests(APITestCase):
 
         response = self.client.get(self.url)
 
-        self.assertEqual(
-            models.University.objects.count(), number_of_universities_to_create
-        )
+        self.assertEqual(University.objects.count(), number_of_universities_to_create)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_retrieve_university(self):
+        university = self.factory()
+        serializer = UniversityModelSerializer(university)
+
+        response = self.client.get(f"{self.url}{university.id}/")
+
+        self.assertEqual(response.data, serializer.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
